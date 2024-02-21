22f626a49fc738391cef8a026845ca7e35955f2e
dexiong <dexiong@staff.sina.com>
2024-02-21 19:06:51 +0800

    update

--- entry/src/main/ets/Card.ts
+++ entry/src/main/ets/Card.ts
@@ -1,15 +1,28 @@
 
 export class Card {
+
     id?: string = undefined;
+    /* 名称 */
     name?: string = "";
+    /* 卡号 */
     card_no?: string = "";
+    /* 更新时间 */
     update?: Date = undefined;
+    /* 过期时间 */
+    deadline?: string = undefined;
+    /* 账单日 */
+    billday?: number = undefined;
+    /* 还款日 */
+    paydate?: number = undefined;
 
     constructor() {
         this.id = undefined;
         this.name = "";
         this.card_no = "";
         this.update = undefined;
+        this.deadline = undefined;
+        this.billday = undefined;
+        this.paydate = undefined;
     }
 
     setId(id) {
@@ -42,5 +55,29 @@ export class Card {
 
     getUpdate() {
         return this.update;
+    }
+
+    setDeadline(deadline) {
+        this.deadline = deadline;
+    }
+
+    getDeadline() {
+        return this.deadline;
+    }
+
+    setBillday(billday) {
+        this.billday = billday;
+    }
+
+    getBillday() {
+        return this.billday;
+    }
+
+    setPaydate(paydate) {
+        this.paydate = paydate;
+    }
+
+    getPaydate() {
+        return this.paydate;
     }
 }
